4da5378328d84e1fd5ecef3185fb5ae45595895c
Boil Marinov <boil.marinov16@gmail.com>
2020-12-08 16:58:13 +0200

    Splitted backend and frontend

--- src/main/java/com/PDOAReplayer/PDOA/Repositories/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.PDOAReplayer.PDOA.Repositories;
-
-import com.PDOAReplayer.PDOA.Entities.Users;
-import org.apache.catalina.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
