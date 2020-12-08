e0185e33b60a2fd23ee529f3a7be35e5ec8573df
Boil Marinov <boil.marinov16@gmail.com>
2020-12-08 16:57:28 +0200

    Splitted backend and frontend

---
+++ PDOA-backend/src/main/java/com/PDOAReplayer/PDOA/PdoaReplayerApplication.java
@@ -0,0 +1,13 @@
+package com.PDOAReplayer.PDOA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdoaReplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdoaReplayerApplication.class, args);
+	}
+
+}
---
+++ PDOA-backend/src/test/java/com/PDOAReplayer/PDOA/PdoaReplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PDOAReplayer.PDOA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdoaReplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
