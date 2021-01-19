7d6b5b689cfa80ba178798f95a73d59eff5fae45
DimityrU <75433184+DimityrU@users.noreply.github.com>
2021-01-19 23:54:57 +0200

    Shows User's Hands in Profile

--- pdoa-ui/src/components/LoggedInIndex.vue
+++ pdoa-ui/src/components/LoggedInIndex.vue
@@ -2,9 +2,10 @@
   <div>
     <h1>Welcome, {{username}}</h1>
     <hr/>
+    <h2 class="border border-dark" style="background: lightyellow; color: black; margin-top: 5px;margin-bottom: 10px; border-radius: 25px; display: inline-block; line-height: 12px; padding: 18px 26px 13px; text-transform: uppercase;">Last Hands</h2>
     <div id="recentHands">
       <div class="row">
-        <HandLink v-for="n in allHands" v-bind:key="n" :first="n.cards[0]" :second="n.cards[1]" :username="n.owner" :handId="n.handId"></HandLink>
+        <HandLink v-for="n in allHands.slice().reverse()" v-bind:key="n" :first="n.cards[0]" :second="n.cards[1]" :username="n.owner" :handId="n.handId"></HandLink>
       </div>
     </div>
   </div>
--- pdoa-ui/src/views/Auth/Profile.vue
+++ pdoa-ui/src/views/Auth/Profile.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="container">
-    <header class="jumbotron">
+  <div>
+    <header class="jumbotron container">
       <h3>
         <img :src="flagURL">
         <strong>{{currentUser.username}}</strong> Profile
       </h3>
     </header>
+    <hr/>
+    <h2 class="border border-dark" style="background: lightyellow; color: black; margin-top: 5px;margin-bottom: 10px; border-radius: 25px; display: inline-block; line-height: 12px; padding: 18px 26px 13px; text-transform: uppercase;">Your Hands</h2>
+      <div class="row">
+        <HandLink v-for="n in ownerHands.slice().reverse()" v-bind:key="n" :first="n.cards[0]" :second="n.cards[1]" :username="n.owner" :handId="n.handId"></HandLink>
+      </div>
   </div>
 </template>
 
 <script>
+import HandsService from '@/services/handsService'
+import HandLink from '@/components/HandLink.vue'
+
 export default {
   name: 'Profile',
   data () {
     return {
+      ownerHands: [],
+      allHands: '',
+      cards: '',
+      username: '',
+      owner: '',
+      handId: '',
       flagURL: ''
     }
   },
@@ -22,11 +36,18 @@ export default {
       return this.$store.state.auth.user
     }
   },
-  mounted () {
+  async mounted () {
+    this.allHands = await HandsService.getAll()
+    this.username = this.$store.state.auth.user.username
     if (!this.currentUser) {
       this.$router.push('/login')
     } else {
       this.flagURL = 'https://www.countryflags.io/' + this.$store.state.auth.user.countryCode + '/flat/32.png'
+    }
+    for (var i = 0; i < 20; i++) {
+      if (this.allHands[i].owner === this.username) {
+        this.ownerHands.push(this.allHands[i])
+      }
     }
   },
   methods: {
@@ -34,6 +55,9 @@ export default {
       this.$store.dispatch('auth/logout')
       this.$router.push('/')
     }
+  },
+  components: {
+    HandLink
   }
 }
 </script>
